b6fc688c0e1c4a4609a3677774e621d5888570d6
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-06-29 18:36:54 +0500

    Added the navigation pages with Icons.

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:instagram/pages/SearchPage.dart';
+import 'package:instagram/pages/TimeLinePage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,10 +13,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool isLoggedIn = false;
+  bool isLoggedIn = true;
+  late PageController pageController;
+  int pageIndex = 0;
 
-  Widget buildHomeScreen() {
-    return Text('Home Page');
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  handlePageChange(int currentPageIndex) {
+    setState(() {
+      pageIndex = currentPageIndex;
+    });
+  }
+
+  Scaffold buildHomeScreen() {
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+          TimeLinePage(),
+          SearchPage(),
+          // UploadPage(),
+          // NotificationPage(),
+          // ProfilePage(),
+        ],
+        controller: pageController,
+        onPageChanged: handlePageChange,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: pageIndex,
+        onTap: (int index) {
+          pageController.animateToPage(index,
+              duration: Duration(milliseconds: 500), curve: Curves.ease);
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_camera),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+        activeColor: Colors.white,
+        inactiveColor: Colors.blueGrey,
+        backgroundColor: Theme.of(context).accentColor,
+      ),
+    );
   }
 
   Scaffold buildSignInScreen() {
